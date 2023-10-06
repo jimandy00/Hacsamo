181119828561648fd40b3262aa65fc6dffdb1e38
newmaruman <140942707+newmaruman@users.noreply.github.com>
2023-10-06 09:51:46 +0900

    [feat] KSH_GestureUI_20231006
    Add Gesture UI

---
+++ Assets/KSH/Gestures/Scripts/PanelGesture.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelGesture : MonoBehaviour
+{
+    [SerializeField] PanelAllGestures panelAllGestures;
+
+
+       
+    
+    public void Show() 
+    {
+        gameObject.SetActive(true);
+
+        GestureManager.AddPanel(gameObject,OnClickExit);
+
+    }
+
+
+
+
+    public void OnClickEdit() 
+    {
+        panelAllGestures.Show(gameObject);
+
+        RectTransform myRectTransform = gameObject.GetComponent<RectTransform>();
+
+        myRectTransform.anchoredPosition = new Vector2(-960,600);
+    }
+
+
+    public void OnClickExit()
+    {
+        GestureManager.RemovePanel(gameObject);
+
+        gameObject.SetActive(false);
+    }
+}
