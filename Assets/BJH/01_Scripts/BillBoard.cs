f4b6ecc2035231cd737fd3fc18c98875684cd7c1
Jihwan Byeon <wkdghk24@gmail.com>
2023-11-02 20:32:34 +0900

    [feat] merge

--- Assets/BJH/01_Scripts/BillBoard.cs
+++ Assets/BJH/01_Scripts/BillBoard.cs
@@ -1,24 +1,25 @@
-using Photon.Pun;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Photon;
-using System.Linq;
-using System.IO.IsolatedStorage;
+//using Photon.Pun;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Photon;
+//using System.Linq;
+//using System.IO.IsolatedStorage;
 
-public class BillBoard : MonoBehaviourPun
-{
-    public Camera cam;
-    void Start()
-    {
+//public class BillBoard : MonoBehaviourPun
+//{
+//    public Camera cam;
 
-    }
+//    void Start()
+//    {
 
-    // Update is called once per frame
-    void Update()
-    {
+//    }
 
-        transform.forward = cam.transform.forward;
+//    // Update is called once per frame
+//    void Update()
+//    {
 
-    }
-}
+//        transform.forward = cam.transform.forward;
+
+//    }
+//}
